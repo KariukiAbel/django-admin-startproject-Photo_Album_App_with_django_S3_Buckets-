1249973cd6c80e99f527953ac537e241b93a17f2
KariukiAbel <abelmuriithi@gmail.com>
2022-02-10 23:04:20 +0300

    done a lot of stuff, from configuration of the MEDIA_URL to STATIC_ROOT to registering my models. I haven't yet added bootstrap but I am going to

--- Photo_Album_App_with_django_S3_Buckets/settings.py
+++ Photo_Album_App_with_django_S3_Buckets/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'photos.apps.PhotosConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Photo_Album_App_with_django_S3_Buckets.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIR = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
