2cb4d2f7c1636970597f5835429c0028d43a03ed
KariukiAbel <abelmuriithi@gmail.com>
2022-02-12 18:22:38 +0300

    Added some code to create a new category. Now I need an add button to the left pane

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Photo
 
 # Create your views here.
@@ -19,6 +19,21 @@ def addPhoto(request):
     if request.method == 'POST':
         data = request.POST
         image = request.FILES.get('image')
+        
+        if data['category'] != 'none':
+            category = Category.object.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(name=data['category_new'])
+        else: 
+            category = None
+            
+        photo = Photo.object.create(
+            category = category,
+            description = data['description'],
+            image = image,
+            
+        )
+        return redirect('gallery')
     
     context = {'categories': categories}
     return render(request, 'photos/add.html', context)
