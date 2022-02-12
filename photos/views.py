f338c428ce5df493fc2e6ae6db46bb310152b702
KariukiAbel <abelmuriithi@gmail.com>
2022-02-12 18:43:06 +0300

    Everything works nicely. I now need to add the styling

--- photos/views.py
+++ photos/views.py
@@ -3,8 +3,14 @@ from .models import Category, Photo
 
 # Create your views here.
 def gallery(request):
+    category = request.GET.get('category')
+    
+    if category == None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name__contains=category)
+    
     categories = Category.objects.all()
-    photos = Photo.objects.all()
     context = {'categories': categories, 'photos' : photos}
     return render(request, 'photos/gallery.html', context)
 
@@ -27,7 +33,7 @@ def addPhoto(request):
         else: 
             category = None
             
-        photo = Photo.object.create(
+        photo = Photo.objects.create(
             category = category,
             description = data['description'],
             image = image,
