4e74feab8e4d1406ea5f21b8109e65dc147ef449
KariukiAbel <abelmuriithi@gmail.com>
2022-02-12 20:48:56 +0300

    Next I want to deploy to a live server

--- photos/views.py
+++ photos/views.py
@@ -27,7 +27,7 @@ def addPhoto(request):
         image = request.FILES.get('image')
         
         if data['category'] != 'none':
-            category = Category.object.get(id=data['category'])
+            category = Category.objects.get(id=data['category'])
         elif data['category_new'] != '':
             category, created = Category.objects.get_or_create(name=data['category_new'])
         else: 
